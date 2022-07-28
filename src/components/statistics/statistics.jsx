d57c0e8f11cfa8098d1922848cec777684f8f8cb
Suprun Oleksandr <suprun.energoatom@gmail.com>
2022-07-28 22:21:58 +0300

    fix Statistics title

--- src/components/statistics/statistics.jsx
+++ src/components/statistics/statistics.jsx
@@ -5,8 +5,7 @@ import css from "./Statistics.module.css";
 const Statistics = ({ title, stats }) => {
   return (
     <section className={css.statistics}>
-      <h2 className={css.title}>{title}</h2>
-
+      {title && <h2 className={css.title}>{title}</h2>}
       <ul className={css.list}>
         {stats.map((stat) => {
           return (
